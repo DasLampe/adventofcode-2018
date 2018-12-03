968e8dacb868b09a9c9bdb9cd1b437ac875f7ee6
André Flemming <andre.flemming@kisters.de>
2018-12-03 13:12:05 +0100

    Day 1 - Part 2

--- src/Day1.java
+++ src/Day1.java
@@ -1,21 +1,31 @@
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.util.Scanner;
-import java.util.stream.Stream;
+import java.util.*;
+
 
 public class Day1 {
     public static void main(String[] args) {
         try {
-            Scanner reader = new Scanner(new File("./src/Day1/input.txt"));
+            List<Integer> results = new ArrayList<Integer>();
+            boolean stop = false;
             int result = 0;
-            while(reader.hasNext()) {
-                result += Integer.parseInt(reader.nextLine());
-            }
+            Scanner reader;
 
-            System.out.println(result);
+            while(stop == false) {
+                reader = new Scanner(new File("./src/Day1/input.txt"));
+
+                while (reader.hasNext()) {
+                    result += Integer.parseInt(reader.nextLine());
+                    if (!results.contains(result)) {
+                        System.out.println(result);
+                        stop = true;
+                        break;
+                    }
+                    results.add(result);
+                }
+                reader.close();
+                reader = null;
+            }
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
